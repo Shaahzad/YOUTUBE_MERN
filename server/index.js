abc1e01ce3e8055afa69b452e227b31846386532
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-06-29 00:34:30 +0500

    Done

--- server/index.js
+++ server/index.js
@@ -20,19 +20,19 @@ app.use(cookieParser())
 app.use(express.json())
 
 
-const corsorigin = {
+const corsOptions  = {
     origin:"https://youtube-mern-three.vercel.app",
     credentials:true,
     optionSuccessStatus:200
 }
 
-app.use(cors(corsorigin))
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://youtube-mern-three.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
+app.use(cors(corsOptions ))
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://youtube-mern-three.vercel.app");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+// });
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
